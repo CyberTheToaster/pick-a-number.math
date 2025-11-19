5ea378ecd5a1ba41ffb96b76a2839b2ab3011eb9
CyberTheToaster <jwozcode123@gmail.com>
2025-11-18 22:28:50 -0500

    add a secret penny

--- src/script.js
+++ src/script.js
@@ -1,4 +1,9 @@
+let secretPenny = document.getElementById("penny")
 let inputfield = document.getElementById("inputnumber")
+let output = document.getElementById("output")
 function findnumber() {
-    alert("your number is " +((inputfield.value*2)+12)/5)
+    output.innerHTML = "your number is " +((inputfield.value*2)+12)/5
+}
+function penny() {
+    secretPenny.hidden = false
 }
